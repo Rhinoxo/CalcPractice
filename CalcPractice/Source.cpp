1456cb62ed894cd2b2bc8f3b125a2a9128bde51d
Rhinoxo <justin@orner.net>
2018-10-09 16:36:31 -0700

    Fixed a problem with the random number limiter and the power calculations.

--- CalcPractice/Source.cpp
+++ CalcPractice/Source.cpp
@@ -4,24 +4,26 @@ By Justin Orner | Last updated on: OCT/9/2018
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 //TODO Functions
+
 int addition(int opLength, bool create)
 {
-	int op1 = rand() % 10 ^ opLength;
-	int op2 = rand() % 10 ^ opLength;
+	int op1 = rand() % static_cast<int>(pow(10, opLength));
+	int op2 = rand() % static_cast<int>(pow(10, opLength));
 	cout << op1 << endl;
 	cout << op2 << endl;
 	return op1 + op2;
-};
+}
 
 int main()
 {
-	srand(83);
+	srand(time(0));
 
 	//TODO Selector
-	cout << addition(5,0) << endl;
+	cout << addition(3,0) << endl;
 
 	system("PAUSE");
 	return 0;
