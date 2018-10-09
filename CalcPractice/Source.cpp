26b1f36a7607a2901edf20dda05465a8ca58e751
Rhinoxo <justin@orner.net>
2018-10-09 13:44:16 -0700

    Testing an addition function and random seeding

--- CalcPractice/Source.cpp
+++ CalcPractice/Source.cpp
@@ -1,2 +1,28 @@
+/*
+By Justin Orner | Last updated on: OCT/9/2018 
+*/
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+//TODO Functions
+int addition(int opLength, bool create)
+{
+	int op1 = rand() % 10 ^ opLength;
+	int op2 = rand() % 10 ^ opLength;
+	cout << op1 << endl;
+	cout << op2 << endl;
+	return op1 + op2;
+};
+
+int main()
+{
+	srand(83);
+
+	//TODO Selector
+	cout << addition(5,0) << endl;
+
+	system("PAUSE");
+	return 0;
+}
